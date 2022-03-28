80a9ea7167a83375ef175e28ccbbf07b233aaabc
Jerry Bopara <jerrybopara@gmail.com>
2022-03-28 18:14:47 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 # use the correct node version for below path
-PATH="$PATH:/home/ubuntu/.nvm/versions/node/v16.13.2/bin/"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+PATH="$PATH:/home/ubuntu/.nvm/versions/node/v16.14.2/bin/"
 cd ~/
+
+echo "Terminating the old app server."
+pm2 kill 
+
+echo "Delete the old code."
+rm -rf ~/awesome-nextapp
+
+echo "Cloning the Code."
 git clone git@github.com:jerrybopara/awesome-nextapp.git
+
+cd ~/awesome-nextapp/
+echo "YARN Install & Build."
 yarn install
 yarn build
 
