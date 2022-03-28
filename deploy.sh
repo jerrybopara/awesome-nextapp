b078ab55ea8515027c1386dc29fff39e1aaa445f
Jerry Bopara <jerrybopara@gmail.com>
2022-03-28 14:03:30 +0530

    changes

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# use the correct node version for below path
+PATH="$PATH:/home/ubuntu/.nvm/versions/node/v16.13.2/bin/"
+# cd ~/awesome-nextapp
+git pull
+yarn install
+yarn build
+
+pm2 start npm --name "nextapp" -- start
+# pm2 restart nextapp
