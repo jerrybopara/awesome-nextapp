b565e7faa972149c09decd77d774cfe6f8b6cb08
Jerry Bopara <jerrybopara@gmail.com>
2022-03-28 16:03:28 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # use the correct node version for below path
 PATH="$PATH:/home/ubuntu/.nvm/versions/node/v16.13.2/bin/"
-# mkdir -p ~/awesome-nextapp
 cd ~/awesome-nextapp
 git pull
 yarn install
